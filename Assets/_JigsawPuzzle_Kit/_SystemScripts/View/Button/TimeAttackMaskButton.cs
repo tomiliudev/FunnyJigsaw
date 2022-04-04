8c2a636d4036dc71dfd71d5e4c14f3d478862780
tomiliudev <tomiliudev@yahoo.co.jp>
2022-04-05 00:29:46 +0900

    TimeAttackボタンの動きを実装

---
+++ Assets/_JigsawPuzzle_Kit/_SystemScripts/View/Button/TimeAttackMaskButton.cs
@@ -0,0 +1,22 @@
+using System;
+using UniRx;
+using UnityEngine;
+
+public sealed class TimeAttackMaskButton : ButtonBase
+{
+    [SerializeField] GameObject _lockText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _onButton = OnTimeAttackMaskButton;
+    }
+
+    void OnTimeAttackMaskButton(Unit _)
+    {
+        _lockText.SetActive(true);
+        Observable.Timer(TimeSpan.FromSeconds(2f))
+            .Subscribe(_ => _lockText.SetActive(false))
+            .AddTo(this);
+    }
+}
